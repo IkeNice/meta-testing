fd98045bd212737e07027133fbc358101917f8f3
IkeNice <ikenice666@gmail.com>
2021-02-17 17:24:50 +0300

    create ProfileList components
    Component that list all profile

---
+++ meta-front/src/views/ProfileList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="profile__list">
+    <h1>Profiles List</h1>
+    <ProfileCard
+      v-for="profile in profile.profiles"
+      :key="profile.id"
+      :profile="profile"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+import ProfileCard from "../components/ProfileCard.vue";
+
+export default {
+  components: {
+    ProfileCard
+  },
+  created() {
+    this.$store.dispatch("profile/fetchAllProfiles");
+  },
+  computed: {
+    ...mapState(["profile"])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.profile__list {
+  display: flex;
+  flex-direction: column;
+  text-decoration: none;
+}
+</style>
