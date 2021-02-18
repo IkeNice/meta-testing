031dddddb4f94d9d065335d01caa6cd74f677116
IkeNice <ikenice666@gmail.com>
2021-02-18 21:14:44 +0300

    update service to work with new api

--- meta-front/src/services/ProfileService.js
+++ meta-front/src/services/ProfileService.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const app_id = "appbssvSnF3Oq5dgI";
-const app_key = "key2opSCUNgWPJ6x0";
+// const app_id = "appbssvSnF3Oq5dgI";
+// const app_key = "key2opSCUNgWPJ6x0";
 
 const apiClient = axios.create({
-  baseURL: "https://api.airtable.com/v0/" + app_id,
+  // baseURL: "https://api.airtable.com/v0/" + app_id,
+  baseURL: "http://127.0.0.1:8000/api/v1/",
   withCreditans: false,
   headers: {
-    Authorization: "Bearer " + app_key,
+    // Authorization: "Bearer " + app_key,
     Accept: "application/json",
     "Content-Type": "application/json"
   }
@@ -15,9 +16,9 @@ const apiClient = axios.create({
 
 export default {
   getAllProfiles() {
-    return apiClient.get("/Psychotherapists");
+    return apiClient.get("/profiles/");
   },
   getProfile(id) {
-    return apiClient.get("/Psychotherapists/" + id);
+    return apiClient.get("/profiles/" + id);
   }
 };
