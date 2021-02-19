038dc69752b0fe612b9eba996f1b4ca1a8871113
IkeNice <ikenice666@gmail.com>
2021-02-19 13:48:18 +0300

    update data transfer from airtable to postgresql. work correctly

--- meta_back/data_transfer.py
+++ meta_back/data_transfer.py
@@ -11,23 +11,27 @@ AIRTABLE_URL = 'https://api.airtable.com/v0/' + APP_ID
 
 
 def db_connection():
+    """ Set up the connection to PostgreSQL """
     postgres_connection = psycopg2.connect("dbname=therapists user=postgres password=admin")
     cursor = postgres_connection.cursor()
     return cursor, postgres_connection
 
 
 def get_data_from_airtable(app_id=APP_ID, app_key=APP_KEY, table=TABLE):
+    """ Get data from Airtable """
     at = airtable.Airtable(app_id, app_key)
     res = at.get(table)
     return res
 
 
 def convert_string(data_to_convert):
+    """ Replace single quote with double quote """
     converted_to_str = str(data_to_convert).replace('\'', '"')
     return converted_to_str
 
 
 def convert_airtable_data(raw_data):
+    """ Convert airtable data from to tuple """
     converted_output = []
     for i, val in enumerate(raw_data['records']):
         methods_list = str(val['fields']['Методы']).replace('\'', '"')
@@ -38,45 +42,50 @@ def convert_airtable_data(raw_data):
 
 
 def get_changes():
-    # данные из таблицы postrgesql
+    """
+        Compare data from PostgreSQL with data from Airtable.
+        If changes detected, then handle it
+    """
+    # data from postgresql
     cur, conn = db_connection()
     cur.execute("select * from therapist_profile_profile")
     old_data = cur.fetchall()
 
-    # данные из таблицы Airtable
+    # data from Airtable
     raw_data = get_data_from_airtable(APP_ID, APP_KEY, TABLE)
     new_data = convert_airtable_data(raw_data)
-    # print(old_data)
-    # print(new_data)
-    diff = list(set(new_data) - set(old_data)) + list(set(old_data) - set(new_data))
+    old_data_id = []
+    new_data_id = []
+    for i in range(len(old_data)):
+        old_data_id.append(old_data[i][0])
+    for i in range(len(new_data)):
+        new_data_id.append(new_data[i][0])
+    # Get difference between old and new data
+    diff = list(set(old_data) - set(new_data)) + list(set(new_data) - set(old_data))
     if diff:
-        #     if len(old_data) == len(new_data):
-        #         # need to be update
-        #         print('same len', diff)
-        #         # update_db_row(diff)
-        #     elif len(old_data) < len(new_data):
-        #         # need to insert
-        #         print('more row in airtable! need to insert\n', diff)
-        #         # insert_in_db(diff)
-        #     else:
-        #         # need to delete
-        #         print('more row in postgresql! need to delete\n', diff)
-        #         # delete_from_db(diff)
         for row in diff:
-            print(row)
+            if not(row[0] in old_data_id):
+                print(row[0], "not in old data, inserting it")
+                insert_in_db(row)
+            elif not(row[0] in new_data_id):
+                print(row[0], "not in new data, deleting it")
+                delete_from_db(row)
+            else:
+                print(row, "in both data, updating it")
+                update_db_row(row)
     else:
-        print('no diff')
+        print('there is no difference')
 
 
 def insert_in_db(data):
+    """ Insert data in table therapist_profile_profile in PostgreSQL """
     try:
         cur, conn = db_connection()
 
-        for _, row in enumerate(data):
-            insert_query = "insert into therapist_profile_profile(name, methods, photo) values (" \
-                           "'" + row[1] + "', '" + row[2] + "', '" + row[3] + "');"
-            cur.execute(insert_query)
-            conn.commit()
+        insert_query = "insert into therapist_profile_profile(id, name, methods, photo) values (" + \
+                       str(data[0]) + ", '" + data[1] + "', '" + data[2] + "', '" + data[3] + "');"
+        cur.execute(insert_query)
+        conn.commit()
 
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -87,13 +96,13 @@ def insert_in_db(data):
 
 
 def delete_from_db(data):
+    """ Delete data from table therapist_profile_profile in PostgreSQL """
     try:
         cur, conn = db_connection()
 
-        for _, row in enumerate(data):
-            delete_query = "delete from therapist_profile_profile where id=" + str(row[0])
-            cur.execute(delete_query)
-            conn.commit()
+        delete_query = "delete from therapist_profile_profile where id=" + str(data[0])
+        cur.execute(delete_query)
+        conn.commit()
 
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -104,26 +113,26 @@ def delete_from_db(data):
 
 
 def update_db_row(data):
-    print(data)
-    # try:
-    #     cur, conn = db_connection()
-    #
-    #     for _, row in enumerate(data):
-    #         update_query = "update therapist_profile_profile " + \
-    #                        " set name=" + row[1] + ", methods=" + row[2] + ", photo=" + row[3] + \
-    #                        "where id=" + row[0]
-    #         cur.execute(update_query)
-    #         conn.commit()
-    #
-    #     cur.close()
-    # except (Exception, psycopg2.DatabaseError) as error:
-    #     print(error)
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
+    """ Update data in table therapist_profile_profile in PostgreSQL """
+    try:
+        cur, conn = db_connection()
+
+        update_query = "update therapist_profile_profile " + \
+                       " set name='" + data[1] + "', methods='" + data[2] + "', photo='" + data[3] + "'" + \
+                       "where id=" + str(data[0])
+        cur.execute(update_query)
+        conn.commit()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
 
 
 def create_new_table():
+    """ Create new table when running this script """
     table_creation_time = round(time.time())
     table_name = 'new_data' + str(table_creation_time)
 
@@ -155,9 +164,9 @@ def create_new_table():
 
 
 def run():
-    pass
+    get_changes()
+    create_new_table()
 
 
 if __name__ == '__main__':
-    # create_new_table()
-    get_changes()
+    run()
